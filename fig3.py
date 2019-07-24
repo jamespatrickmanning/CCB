d983cd93d79b7357445ee0fe52d3993adac755f7
jamespatrickmanning <james.manning@noaa.gov>
2019-07-24 11:01:49 -0400

    added new data  (1979-2016) from Bob P

--- fig3.py
+++ fig3.py
@@ -12,8 +12,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #HARDCODES
-time=['1991','1992','1993','1999','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015']
-num=[20, 30,2, 149,94,259,95,36,55,88,43,85,182,234,138,408,224,1241,513]# extracted from stranding data for J. Manning.xls
+#time=['1991','1992','1993','1999','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015']
+#num=[20, 30,2, 149,94,259,95,36,55,88,43,85,182,234,138,408,224,1241,513]# extracted from stranding data for J. Manning.xls
+# the following comes from Bob Prescott in Feb 2019
+time=range(1979,2019)
+num=[12,22,22,6,6,33,19,17,39,10,37,45,19,31,44,34,178,14,57,46,323,49,96,201,92,37,53,91,39,89,204,232,152,413,204,1241,613,476,420,818] # from Bob Prescott email in Feb 2019
+
 method='new' # method of plotting where 'old' method used "Pandas"
 
 fig,axes=plt.subplots(1,1,figsize=(8,4))
